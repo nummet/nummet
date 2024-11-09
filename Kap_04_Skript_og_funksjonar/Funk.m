7613c7b7b08236011ffd2d4911476aef717a1a9c
Martin Lilleeng Sætra <martinls@met.no>
2024-11-09 13:36:24 +0100

    Opprydding og ny struktur.

--- Kap_04_Skript_og_funksjonar/Funk.m
+++
@@ -1,6 +0,0 @@
-function F=Funk(x)
-
-% Funksjonen f(x)=x^2 cos x.
-% Funksjonen tar berre skalarar som input.
-
-F=x^2*cos(x);
--- Kap_19_Redusert_trappeform/ByteRekker.m
+++
@@ -1,9 +0,0 @@
-function M=ByteRekker(A,m,n)
-
-% Funksjon som byter om to rekker i ei matrise
-% Input:
-% A: Matrisa som skal endrast.
-% m og n: Rekkene som skal bytast om.
-
-M=A;
-M([m n],:)=A([n m],:);
