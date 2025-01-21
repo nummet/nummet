c1bd06cf57af4fdbc188ff1259679b46cc7c73fe
Sølve Selstø <124062842+Soelve@users.noreply.github.com>
2025-01-21 10:30:19 +0100

    Update Oppgaave_7_2_for.py
    Fiksa enda meir rusk

--- Kap_07_Aa_loeyse_likningar/Oppgaave_7_2_for.py
+++ Kap_07_Aa_loeyse_likningar/Oppgaave_7_2_for.py
@@ -1,7 +1,7 @@
 """Dette er ei implementering av Newtons metode der iterasjonane blir
 gjort i ei for-løkke.
-Startverdi, presisjon, funksjonsuttrykk og den deriverte av denne er input.
-Den estimerte løysinga blir skrive til skjerm.
+Startverdi, antal iterasjonar, funksjonsuttrykk og den deriverte av denne er 
+input. Den estimerte løysinga blir skrive til skjerm.
 """
 
 # Inporterer numpy
@@ -11,21 +11,16 @@ import numpy as np
 def funk(x):
    return np.sqrt(x)-np.cos(x)
    
-# Den deriverte av funksjonsuttrykket
-def funk_deriv(x):
-    return 1/(2*np.sqrt(x))+np.sin(x)
     
 # Bestemmer x_0
-x = 1
+x = 3
 
 # Antal iterasjonar
-N = 5
+N = 7
 
 for n in range(1, N+1):
-    # Kopierer den gamle x
-    x_gml = x
     # Iterasjonsskjema
-    x = x-funk(x)/funk_deriv(x)
+    x = funk(x)
 
 # Skriv svaret til skjerm
 print(f'x = {x:.5f}')
